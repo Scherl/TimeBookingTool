6bd064c870d986036dc0150e5cb0373b66f9f3cb
Sabina Scherle <sabina.scherle@outlook.de>
2023-03-23 16:25:37 +0100

    pensum business component

---
+++ TimeBooking/Data/Services/BusinessComponents/PensumBusinessComponent.cs
@@ -0,0 +1,21 @@
+﻿using TimeBooking.Data.Interfaces;
+
+namespace TimeBooking.Data.Services.BusinessComponents
+{
+    public class PensumBusinessComponent
+    {
+        private readonly IPensumService _pensumService;
+
+        public PensumBusinessComponent(IPensumService pensumService)
+        {
+            _pensumService = pensumService;
+        }
+
+        public async Task<decimal> GetWeeklyWorkloadByEmployeeAsync(Guid id)
+        {
+            if (id == Guid.Empty) return decimal.Zero;
+            var pensum = await _pensumService.GetWeeklyWorkloadByEmployeeAsync(id);
+            return pensum;
+        }
+    }
+}
---
+++ UnitTestTimeBooking/PensumServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TimeBooking.Data.Interfaces;
+using TimeBooking.Data.Services.BusinessComponents;
+using Xunit.Sdk;
+using static Xunit.Assert;
+
+namespace UnitTestTimeBooking
+{
+    public class PensumServiceTest
+    {
+        private readonly IPensumService _context;
+        private readonly Mock<IPensumService> _mockbuilder;
+
+        public PensumServiceTest()
+        {
+            _mockbuilder = new Mock<IPensumService>();
+            _context = _mockbuilder.Object;
+        }
+
+        [Fact(DisplayName = "WeeklyWorkloadByEmployee")]
+        public async Task TestWeeklyWOrkloadByEmployee()
+        {
+            //arrange
+            var workload = 0.8m;
+            var id = Guid.NewGuid();
+            PensumBusinessComponent service = new(_context);
+
+            _mockbuilder.Setup(x => x.GetWeeklyWorkloadByEmployeeAsync(id)).ReturnsAsync(workload);
+
+            // act
+            var result = await service.GetWeeklyWorkloadByEmployeeAsync(id);
+
+            //assert
+            IsType<decimal>(result);
+            Single(result.ToString());
+            _mockbuilder.VerifyAll();
+        }
+    }
+}
