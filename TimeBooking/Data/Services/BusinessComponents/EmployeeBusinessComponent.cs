615659666fc3aeb64250f9241951fd240bda3686
Sabina Scherle <sabina.scherle@outlook.de>
2023-03-23 16:07:03 +0100

    Employee Business Component

---
+++ TimeBooking/Data/Services/BusinessComponents/EmployeeBusinessComponent.cs
@@ -0,0 +1,21 @@
+﻿using TimeBooking.Data.Interfaces;
+using TimeBooking.Data.Models;
+
+namespace TimeBooking.Data.Services.BusinessComponents
+{
+    public class EmployeeBusinessComponent
+    {
+        private readonly IEmployeeService _employeeService;
+
+        public EmployeeBusinessComponent(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public async Task<Employee> GetMitarbeiterByNameAsync(string name)
+        {
+            var employee = await _employeeService.GetMitarbeiterByNameAsync(name);
+                return employee;
+        }
+    }
+}
